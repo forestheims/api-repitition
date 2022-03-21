89233dc2967899b0929570c74cb2a13ff48e1db8
forestheims <forestheims@gmail.com>
2022-03-20 22:35:24 -0700

    test created for get all route of resins

--- __tests__/resins.test.js
+++ __tests__/resins.test.js
@@ -13,18 +13,39 @@ describe('api-repitition routes', () => {
     pool.end();
   });
 
+  const expected = {
+    name: 'Frankincense',
+    sourceTaxonomy: 'Boswellia sacra',
+    predominantChemicals: [
+      'acid resin',
+      'gum',
+      'alpha-boswellic acid',
+      'phellandrene',
+    ],
+  };
+  const expectedBackwords = {
+    name: 'Esnecniknarf',
+    sourceTaxonomy: 'Arcas aillewsob',
+    predominantChemicals: [
+      'Nisen dica',
+      'mug',
+      'Dica cillewsob-ahpla',
+      'enerddnallehp',
+    ],
+  };
+
   it('inserts a row into the resins table', async () => {
-    const expected = {
-      name: 'Frankincense',
-      sourceTaxonomy: 'Boswellia sacra',
-      predominantChemicals: [
-        'acid resin',
-        'gum',
-        'alpha-boswellic acid',
-        'phellandrene',
-      ],
-    };
     const res = await request(app).post('/api/v1/resins').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('returns all rows from the resins table', async () => {
+    await request(app).post('/api/v1/resins').send(expected);
+    await request(app).post('/api/v1/resins').send(expectedBackwords);
+    const res = await request(app).get('/api/v1/resins');
+    expect(res.body).toEqual([
+      { id: expect.any(String), ...expected },
+      { id: expect.any(String), ...expectedBackwords },
+    ]);
   });
 });
