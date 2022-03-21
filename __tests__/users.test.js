9b9ca87231e4ccc8240d6d2591526eabad103a5e
forestheims <forestheims@gmail.com>
2022-03-21 08:44:27 -0700

    approaching the users resource horizontally

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -25,4 +25,35 @@ describe('api-repitition routes', () => {
     const res = await request(app).post('/api/v1/users').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
+
+  it('gets all rows from the users table', async () => {
+    await request(app).post('/api/v1/users').send(expected);
+    const res = await request(app).get('/api/v1/users');
+    expect(res.body).toEqual([{ id: expect.any(String), ...expected }]);
+  });
+
+  it('gets a row by id from the users table', async () => {
+    await request(app).post('/api/v1/users').send(expected);
+    const res = await request(app).get('/api/v1/users/1');
+    expect(res.body).toEqual({ id: '1', ...expected });
+  });
+
+  it('updates a row by id on the users table', async () => {
+    await request(app).post('/api/v1/users').send(expected);
+    const res = await request(app)
+      .patch('/api/v1/users/1')
+      .send({ username: 'silicon dioxide' });
+    expect(res.body).toEqual({
+      id: '1',
+      ...expected,
+      username: 'silicon dioxide',
+    });
+  });
+
+  it('deletes a row by id from the users table', async () => {
+    await request(app).post('/api/v1/users').send(expected);
+    const getById = await User.getById(1);
+    const res = await request(app).delete(`/api/v1/users/${getById.id}`);
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
 });
