b063f1f787c661968eed998ca78f1ec74123996b
forestheims <forestheims@gmail.com>
2022-03-21 08:57:32 -0700

    users resource TDD passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -23,19 +23,19 @@ describe('api-repitition routes', () => {
 
   it('adds a row to the users table', async () => {
     const res = await request(app).post('/api/v1/users').send(expected);
-    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+    expect(res.body).toEqual({ userId: expect.any(String), ...expected });
   });
 
   it('gets all rows from the users table', async () => {
     await request(app).post('/api/v1/users').send(expected);
     const res = await request(app).get('/api/v1/users');
-    expect(res.body).toEqual([{ id: expect.any(String), ...expected }]);
+    expect(res.body).toEqual([{ userId: expect.any(String), ...expected }]);
   });
 
   it('gets a row by id from the users table', async () => {
     await request(app).post('/api/v1/users').send(expected);
     const res = await request(app).get('/api/v1/users/1');
-    expect(res.body).toEqual({ id: '1', ...expected });
+    expect(res.body).toEqual({ userId: '1', ...expected });
   });
 
   it('updates a row by id on the users table', async () => {
@@ -44,7 +44,7 @@ describe('api-repitition routes', () => {
       .patch('/api/v1/users/1')
       .send({ username: 'silicon dioxide' });
     expect(res.body).toEqual({
-      id: '1',
+      userId: '1',
       ...expected,
       username: 'silicon dioxide',
     });
@@ -53,7 +53,7 @@ describe('api-repitition routes', () => {
   it('deletes a row by id from the users table', async () => {
     await request(app).post('/api/v1/users').send(expected);
     const getById = await User.getById(1);
-    const res = await request(app).delete(`/api/v1/users/${getById.id}`);
-    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+    const res = await request(app).delete(`/api/v1/users/${getById.userId}`);
+    expect(res.body).toEqual({ userId: expect.any(String), ...expected });
   });
 });
